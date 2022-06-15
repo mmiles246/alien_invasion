089247ba3cd4266890e144f0d8a9d32fd154fa36
Michael <michael.miles2468@gmail.com>
2022-06-15 11:30:24 -0400

    Ship moves continuously left and right, added window boundries on x-axis.

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,14 +23,41 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            #Redrawing screen through each loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+
+    def _check_events(self):
+        """Responds to keystrokes and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame. K_LEFT:
+            self.ship.moving_left = False
+
+    def  _update_screen(self):
+        """Updates images and screen and flips to new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
 
 # if _name_ == '_main_':
 ai=AlienInvasion()
